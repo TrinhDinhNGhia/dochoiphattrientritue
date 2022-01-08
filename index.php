f1911555b1aaedebf62478b9464a30f9168990e8
TrinhDinhNGhia <90976313+TrinhDinhNGhia@users.noreply.github.com>
2022-01-08 10:34:14 +0700

    demo

---
+++ index.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Web đồ chơi trí tuệ</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+         
+         </div><div class="menu">
+           <ul class="list_menu">
+               <li><a href="index.php">Trang chủ</a> </li>
+               <li><a href="index.php?quanly=danhmucsanpham&id=1">Danh mục sản phẩm</a> </li>
+               <li><a href="index.php?quanly=giohang">Giỏ hàng</a> </li>
+               <li><a href="index.php?quanly=tintuc">Tin tức</a> </li>
+               <li><a href="index.php?quanly=lienhe">Liên hệ</a> </li>
+           </ul>
+        </div><div id="main">
+    <div class="sidebar">
+                <ul class="list_sidebar">
+                     <li><a href="index.php?quanly=danhmucsanpham&id=1">Sản phẩm cho bé</a> </li>
+                     <li><a href="index.php?quanly=danhmucsanpham&id=2">Truyện tranh</a> </li>
+                      <li><a href="index.php?quanly=danhmucsanpham&id=3">
+Sách tiếng Việt khác</a> </li>
+                       <li><a href="#">Bộ đồ chơi phát triển trí tuệ toán học cho bé</a> </li>
+                     <li><a href="#">Khối Gỗ Đồ Chơi Phát Triển Trí Tuệ Cho Bé</a> </li>
+               </ul>
+               </div>            
+            
+    <div class="maincontent">
+        <h3>Sản phẩm mới nhất</h3>
+               <ul class="product_list">
+                   <li>
+                       <a href="">
+                           <img src="img/rubik.jpg">
+                           <p class="title_product">Tên sản phẩm : Rubik</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+
+                       </a>
+                   </li>
+
+                   <li>
+                       <img src="img/do-choi-tui-xep-hinh-cong-truong-vjg024-1.jpg">
+                       <p class="title_product">Tên sản phẩm : Rubik</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+
+                   <li>
+                       <img src="img/phi-co-co-den-cy-7709-3.jpg">
+                       <p class="title_product">Phi cơ có đèn CY.7709</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+
+                   <li>
+                       <img src="img/do-choi-bong-nhua-pu-mem-5inch-cy-1610a-1.jpg">
+                       <p class="title_product">Đồ chơi bóng nhựa </p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+
+                   <li>
+                       <img src="img/xe-chay-da-hinh-minion-cy-338-6-1.jpg">
+                       <p class="title_product">Xe chạy đà hình Minion</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+
+                   <li>
+                       <img src="img/do_choi_dan_vit_me_con_cy.039_1.jpg">
+                       <p class="title_product">Bộ đồ chơi đàn vịt con và vịt mẹ</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+
+                   <li>
+                       <img src="img/dan-trau-no-6600-3_1.jpg">
+                       <p class="title_product">Đồ chơi đàn nhạc hình chú trâu</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+
+                   <li>
+                       <img src="img/do-choi-dan-nhac-hinh-tho-cy-3300-1.jpg">
+                       <p class="title_product">Đồ chơi đàn nhạc hình thỏ</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+
+                   <li>
+                       <img src="img/bang-ve-cat-cy-dq634-01.jpg">
+                       <p class="title_product">Bảng vẽ cát CY.DQ634</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+
+                   <li>
+                       <img src="img/xe_oto_bien_hinh_im.66101-1_1.jpg">
+                       <p class="title_product">Xe ôtô biến hình IM.66101-1</p>
+                           <p class="price_product">Giá :300000 vnd</p>
+                   </li>
+               </ul>              
+    </div>
+
+</div>
+<div class="clear"></div>
+        <div class="footer">
+            <p class="footer_copyright">Coppyright by trinh Nghia develpoer 2000</p>
+         </div>       
+    </div>
+    
+</body>
+</html>
